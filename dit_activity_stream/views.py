225acc6a93de2dce0e3cad9cdec3e7c0337cb978
Nawaz <nawaz.ahmed@digital.trade.gov.uk>
2022-01-20 09:37:45 +0000

    Settings controlled view functionality added

--- dit_activity_stream/views.py
+++ dit_activity_stream/views.py
@@ -1,10 +1,27 @@
+from django.conf import settings
 from django.contrib.auth.models import User
 from django.http import JsonResponse
+from django.utils.module_loading import import_string
 from django.views import View
 
 
-def get_activity_stream_view():
-    return DitActivityStreamView.as_view()
+# We want this function to return a View
+def get_activity_stream_view() -> View:
+    """
+    Get the view from the RENDER_VIEW setting
+    """
+
+    # Use default view if RENDER_VIEW setting doesn't exist.
+    if not getattr(settings, "RENDER_VIEW", None):
+        return DitActivityStreamView.as_view()
+
+    render_view_class = import_string(settings.RENDER_VIEW)
+
+    # Check if subclass of View
+    if not issubclass(render_view_class, View):
+        raise ValueError("RENDER_VIEW must inherit from View")
+
+    return render_view_class.as_view()
 
 
 class DitActivityStreamView(View):
